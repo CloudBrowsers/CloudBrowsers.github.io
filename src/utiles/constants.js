935385bbf0dcf2ad30dec5da117428c88db4735c
tanmay <agarwal.tanmay@tftus.com>
2023-08-28 17:44:03 +0530

    changes in baseurl

--- src/utiles/constants.js
+++ src/utiles/constants.js
@@ -1,8 +1,8 @@
 // export const jira_api = process.env.REACT_APP_JIRA_VARIABLE;
 
 export const jira_api = `https://app.cloudifytests.io/contactusform`;
-export const free_trial_api = `https://app.cloudifytests.com/send-invite-mail/`;
+export const free_trial_api = `https://app.cloudifytests.io/send-invite-mail/`;
 export const customer_support_api =
   "https://cloudifytests.atlassian.net/servicedesk/customer/portal/3";
 export const ns_verification_api =
-  "https://app.cloudifytests.com/accept-invite-mail/";
+  "https://app.cloudifytests.io/accept-invite-mail/";
