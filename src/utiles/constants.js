08aff61466ed037d559aacca9f740f90dbc4fbba
tanmay <agarwal.tanmay@tftus.com>
2023-11-15 13:56:34 +0530

    changes for free trial and contact us url

--- src/utiles/constants.js
+++ src/utiles/constants.js
@@ -1,8 +1,8 @@
 // export const jira_api = process.env.REACT_APP_JIRA_VARIABLE;
 
-export const jira_api = `https://app.cloudifytests.io/contactusform`;
-export const free_trial_api = `https://app.cloudifytests.io/send-invite-mail/`;
+export const jira_api = `https://tft.cloudifytests.io/contactusform`;
+export const free_trial_api = `https://tft.cloudifytests.io/send-invite-mail/`;
 export const customer_support_api =
   "https://cloudifytests.atlassian.net/servicedesk/customer/portal/3";
 export const ns_verification_api =
-  "https://app.cloudifytests.io/accept-invite-mail/";
+  "https://tft.cloudifytests.io/accept-invite-mail/";
